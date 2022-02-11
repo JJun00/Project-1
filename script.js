a75de07fa6ff6472958567eaf3e9880b2bf23ab4
Josh <josh@Joshs-MacBook-Pro.local>
2022-02-11 16:10:20 -0500

    completed fetching data for country and weather data just need to add weather icon. Also changed a few of the classes from html body into ids for some of the functions to work

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let country = document.getElementById("countryName");
-let dayWeatherCode = "";
-let nightWeatherCode = "";
-let weather = "";
-// let capital = document.getElementById("capitalName");
+let country = document.getElementById("countryName")
+let countryName = country.getAttribute("value");
 
 // generate random country
 async function randomLocation() {
@@ -10,45 +7,57 @@ async function randomLocation() {
     let info = await response.json();
     let index = Math.floor(Math.random() * info.data.length);
     let randomCountry = info.data[index].name;
-    // let randomCapital = info.data[index].capital
-    country.setAttribute("value", randomCountry)
-    // capital.setAttribute("value", randomCapital)
-    // console.log(randomCountry, randomCapital);
+    country.setAttribute("value", randomCountry);
+    countryName = randomCountry;
 }
-
 
 // get weather data of country
 async function getWeather() {
-    if (country.value === "country") {
-        console.log("Randomize country first to get weather forecast!");
-    }
-    let response = await fetch('http://api.weatherapi.com/v1/current.json?key=e8c9a942f1b642e29a0155234220902&q=%{country}&aqi=yes');
+    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=e8c9a942f1b642e29a0155234220902&q=${countryName}&aqi=yes`);
     let info = await response.json();
-    fahrenheitWeather = info.current.temp_f;
-    dayWeatherCode = info.current.condition.code;
-    console.log(typeof dayWeatherCode);
+    // console.log(countryName);
+    dayWeatherCode = await info.current.condition.code;
+    fahrenheitWeather = await info.current.temp_f;
+    console.log(fahrenheitWeather)
     console.log(dayWeatherCode)
-    // await weatherData();
+    getData();
+    displayData();
 }
-
-async function compareData() {
+// get weather code and icon 
+async function getData() {
     let response = await fetch("https://www.weatherapi.com/docs/weather_conditions.json")
     let info = await response.json();
     for (let i = 0; i < info.length; i++) {
         if (info[i].code === dayWeatherCode) {
-            console.log(typeof info[i].code);
+            // console.log("it exists");
         }
     }
 }
-compareData();
-
 // displaying weather data on html 
-function weatherData() {
-    let div = document.createElement("div")
-    let text = "'Location: ${country}'"
-    div.classList.add("weatherForecast");
-    let div1 = document.createElement("div");
-    let div2 = document.createElement("div");
-    // day & night weather icons
-    let dayWeatherIcon = document.createElement("img")
+function displayData() {
+    displayCountry = document.getElementById("countryDisplay");
+    displayWeather = document.getElementById("currentWeather");
+    clearData();
+    let h1 = document.createElement("h1");
+    let h2 = document.createElement("h1");
+    h1Text = document.createTextNode(`Country : ${countryName}`);
+    h1.appendChild(h1Text);
+    displayCountry.appendChild(h1);
+    h1Text2 = document.createTextNode(`Current Temp: ${fahrenheitWeather}°F`);
+    h2.appendChild(h1Text2);
+    displayWeather.appendChild(h2);
+}
+
+// set condition only when previous weather data still exists.
+function clearData() {
+    if (displayCountry.childNodes != 0 && displayWeather.childNodes != 0) {
+        removeNodes(displayCountry);
+        removeNodes(displayWeather);
+    }
+}
+// function to remove html elements after fetching weather data from previous country
+function removeNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
